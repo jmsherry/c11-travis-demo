1a9f5187b3aae30a9e5d886867223b4514e8e59e
James Sherry <james.m.sherry@googlemail.com>
2022-09-08 20:45:47 +0100

    initial files

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+// app.use((req, res, next) => {
+//   console.log('hit')
+//   next();
+// })
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/api/v1/message", (req, res) => {
+  return res.status(200).json("Special Message");
+});
+
+export default app;
